a048d414d86ac0f31099ae99306894884895155c
waleed11011 <445050004@std.psau.edu.sa>
2026-05-02 02:52:21 +0300

    Task 1: Added lock to protect counters and avoid race conditions

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
-
+import java.util.concurrent.locks.ReentrantLock; // Task 1 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
     public static final String RESET = "\u001B[0m";
@@ -38,7 +38,9 @@ class SharedResources {
     
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
-    
+
+    public static final ReentrantLock counterLock = new ReentrantLock(); // Task 1
+
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
     
@@ -46,21 +48,35 @@ class SharedResources {
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
+            counterLock.lock(); // Task 1: 
+        try {
         contextSwitchCount++;
+        } finally {
+            counterLock.unlock(); // Task 1:
+        }
+
     }
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
+        counterLock.lock(); // Task 1
+    try {
         completedProcessCount++;
+    } finally {
+        counterLock.unlock(); // Task 1    
     }
-    
+    }
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
+    counterLock.lock(); // Task 1
+    try {    
         totalWaitingTime += time;
+    } finally {
+        counterLock.unlock(); // Task 1
     }
-    
+    }
     // Method to log execution
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
